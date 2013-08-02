4bcdb8c74ff4da3ee822fc98f43e4405d3c637a4
Paul Andrieux <paul@appventus.com>
2013-08-02 17:15:34 +0200

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Execute syntax and phpunit tests
+if [ -f .git/hooks/pre-commit ]; then
+    if grep -q ".git/hooks/appventus.pre-commit.tester" .git/hooks/pre-commit; then
+    else
+        cat vendor/appventus/hooks/pre-commit.add.sample >> .git/hooks/pre-commit
+    fi
+else
+    touch .git/hooks/pre-commit
+    cat vendor/appventus/hooks/pre-commit.sample >> .git/hooks/pre-commit
+fi
