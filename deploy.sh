feb50893b969ba9dec653815199ee8e25eb2976a
Paul Andrieux <paul@appventus.com>
2013-08-02 18:56:32 +0200

    fix paths

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # Execute syntax and phpunit tests
-if [ -f .git/hooks/pre-commit ]; then
-    if grep -q ".git/hooks/appventus.pre-commit.tester" .git/hooks/pre-commit; then
-    else
-        cat vendor/appventus/hooks/pre-commit.add.sample >> .git/hooks/pre-commit
+if [ -f ../../../.git/hooks/pre-commit ]; then
+    if ! grep -q ".git/hooks/appventus.pre-commit.tester" ../../../.git/hooks/pre-commit; then
+        cat pre-commit.add.sample >> ../../../.git/hooks/pre-commit
     fi
 else
-    touch .git/hooks/pre-commit
-    cat vendor/appventus/hooks/pre-commit.sample >> .git/hooks/pre-commit
+    touch ../../../.git/hooks/pre-commit
+    cat pre-commit.sample >> ../../../.git/hooks/pre-commit
 fi
